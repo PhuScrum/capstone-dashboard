515cb894571fed8700ca6333c10a6dbe8151d752
Hien Nguyen <40570309+hiennn281@users.noreply.github.com>
2020-08-23 20:56:59 +0700

    Revert "Revert "*mockup* model present""
    This reverts commit fce9ad4481946bed9a64d45ab8af7791b586f74c.

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -5,12 +5,18 @@ import { ModelSelectComponent } from './model-select/model-select.component';
 import { ModelPresentComponent } from './model-present/model-present.component';
 
 import { NgxEchartsModule } from 'ngx-echarts';
+import { NzProgressModule } from 'ng-zorro-antd/progress';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 
 
 @NgModule({
   declarations: [ModelPageComponent, ModelSelectComponent, ModelPresentComponent],
   imports: [
     CommonModule,
+    NzProgressModule,
+    NzGridModule,
+    NzSelectModule,
     NgxEchartsModule.forRoot({
       /**
        * This will import all modules from echarts.
