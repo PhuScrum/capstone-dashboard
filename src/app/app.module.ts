e18c1dcdb45de42b2e6381799239827c0910b7da
PhuScrum <s3686970@rmit.edu.vn>
2020-08-23 19:18:20 +0700

    *mockup* model present

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NavbarModule} from './navbar/navbar.module'
-import {ModelModule} from './model/model.module'
+import {ModelModule} from './model/model.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en'
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -15,8 +25,12 @@ import {ModelModule} from './model/model.module'
     AppRoutingModule,
     NavbarModule,
     ModelModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzButtonModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
