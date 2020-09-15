7e7e2c07e67c6223f8cbf07353129b8c3191a3fc
anhtubui1204 <s3634162@rmit.edu.vn>
2020-09-15 13:38:02 +0700

    Update model-present.component.ts
    add create hash map function

--- App/src/app/model/model-present/model-present.component.ts
+++ App/src/app/model/model-present/model-present.component.ts
@@ -9,6 +9,7 @@ import * as data from './model_random_forest.json'
 
 import { ModelService } from '../model.service';
 import { Data as DataType } from 'src/data/dataType';
+import { error } from '@angular/compiler/src/util';
 
 
 @Component({
@@ -16,7 +17,7 @@ import { Data as DataType } from 'src/data/dataType';
   templateUrl: './model-present.component.html',
   styleUrls: ['./model-present.component.css']
 })
-export class ModelPresentComponent implements OnInit {
+export class ModelPresentComponent implements OnInit, OnChanges {
     @Input() model!: DataType;
 
     currentModel: any;
@@ -24,12 +25,13 @@ export class ModelPresentComponent implements OnInit {
 
   chartOption: EChartOption = {
 
-  };
+  }
 
   dataList: DataType[] = [];
 
   data = data
   taget= data.taget
+
   r2Score= parseFloat(data.R2_Score) * 100
   rmse= parseFloat(data.RMSE).toFixed(2)
   state= data.data.state
@@ -41,51 +43,88 @@ export class ModelPresentComponent implements OnInit {
   }
   constructor() {
     this.data = data
-    this.chartOption = {
-      title: {
-          text: 'Agtuary model presentation'
-      },
-      tooltip: {
-          trigger: 'axis'
-      },
-      legend: {
-          data: ['expense', 'revenue']
-      },
-      grid: {
-          left: '0%',
-          right: '0%',
-          bottom: '3%',
-          containLabel: true
-      },
-      toolbox: {
-          feature: {
-              saveAsImage: {}
-          }
-      },
-      xAxis: {
-          type: 'category',
-          boundaryGap: false,
-          data: ['2010', '2011', '2012', '2013', '2014', '2015', '2016']
-      },
-      yAxis: {
-          type: 'value'
-      },
-      series: [
-          {
-              name: 'expense',
-              type: 'line',
-              stack: 'quantity',
-              data: [320, 332, 301, 334, 390, 330, 320]
-          },
-          {
-              name: 'revenue',
-              type: 'line',
-              stack: 'quantity',
-              data: [820, 932, 901, 934, 1290, 1330, 1320]
-          }
-      ]
-  };
 
+  }
+
+  totalYearAxis(year1: number[] , year2: number[]): number[] {
+    let totalYear = [];
+    totalYear = year1.concat(year2);
+
+    const result = Array.from(new Set(totalYear));
+    const sortedYear = result.sort(function(a, b){return a-b});
+
+    return sortedYear;
+  }
+
+  createHashMap(year: number[], values: number[]): any[]{
+    let seriesData = [];
+    let unitSerie = [];
+
+    if (year.length !== values.length) {
+        throw error('Length must be the same')
+    } else {
+        for (let i = 0; i < year.length; i ++) {
+            unitSerie = [year[i].toString(), values[i]];
+            seriesData.push(unitSerie);
+        }
+
+        return seriesData;
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+     console.log(this.createHashMap(this.model.data_by_crops[0].data.year, this.model.data_by_crops[0].data.production))
+    this.chartOption = {
+        title: {
+            text: 'Agtuary model presentation'
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            data: ['real', 'prediction']
+        },
+        grid: {
+            left: '0%',
+            right: '0%',
+            bottom: '3%',
+            containLabel: true
+        },
+        toolbox: {
+            feature: {
+                saveAsImage: {}
+            }
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data:  this.totalYearAxis(this.model.data_by_crops[0].data.year, this.model.data_by_crops[0].prediction.year)
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                name: 'real',
+                type: 'line',
+                stack: 'quantity',
+                // data: this.model.data_by_crops[0].data.production
+                data: this.createHashMap(this.model.data_by_crops[0].data.year, this.model.data_by_crops[0].data.production)
+            },
+            {
+                name: 'prediction',
+                type: 'line',
+                stack: 'quantity',
+                lineStyle: {
+                    type: 'dashed'
+                },
+                
+                data: this.createHashMap(this.model.data_by_crops[0].prediction.year, this.model.data_by_crops[0].prediction.production)
+
+
+            }
+        ]
+    };
   }
 
   ngOnInit(): void {
