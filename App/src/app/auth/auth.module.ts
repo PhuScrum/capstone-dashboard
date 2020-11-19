348c27c55f2149de1b3097f7598d5bede8020bfb
anhtubui1204 <s3634162@rmit.edu.vn>
2020-11-19 16:07:05 +0700

    add form for login page

--- App/src/app/auth/auth.module.ts
+++ App/src/app/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginPageComponent } from './login-page/login-page.component'
-
+import { LoginPageComponent } from './login-page/login-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [LoginPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   exports: [ LoginPageComponent ]
 })
--- App/src/app/auth/login-page/login-page.component.ts
+++ App/src/app/auth/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
+  form: FormGroup
+
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, { validators: [Validators.required, Validators.minLength(3)] }),
+      password: new FormControl(null, { validators: [Validators.required, Validators.minLength(3)] })
+    });
+  }
+
+  onLogin() {
+    if (this.form.invalid) return;
+    console.log(this.form.value)
   }
 
 }
