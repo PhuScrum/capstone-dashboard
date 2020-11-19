7d048bfc958584c0237a319835661e49c90d3167
anhtubui1204 <s3634162@rmit.edu.vn>
2020-11-19 19:49:14 +0700

    Update dbConfig.js

--- AppBackend/dbConfig.js
+++ AppBackend/dbConfig.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 
+const key = 'fnAD7ADnJXACDd2V6pO3pXItGGVXn9FCIQVww8D0'
+
 //connect to faunadb
 const faunadb = require('faunadb');
-const secret = process.env.FAUNADB_SECRET_KEY;
+const secret = key;
 const q = faunadb.query;
 const client = new faunadb.Client({ secret }); //guest client
 
