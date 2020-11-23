606e60ed7d5132a32b1468997138b9356ab7e206
anhtubui1204 <s3634162@rmit.edu.vn>
2020-11-23 11:10:19 +0700

    add profile components

--- App/src/app/profile/profile.module.ts
+++ App/src/app/profile/profile.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { ProfilePageComponent } from './profile-page.component';
+import { ModelsComponent } from './models/models.component';
+import { DatasetsComponent } from './datasets/datasets.component';
+import { ProfileSummaryComponent } from './profile-summary/profile-summary.component';
 
 @NgModule({
-  declarations: [ProfilePageComponent],
+  declarations: [ProfilePageComponent, ModelsComponent, DatasetsComponent, ProfileSummaryComponent],
   imports: [
     CommonModule
   ]
