5f15ff18562242b6bcf4c1c1a5583f3d184a213b
anhtubui1204 <s3634162@rmit.edu.vn>
2020-12-11 22:28:07 +0700

    update readfile

--- AppBackend/controller/dataset/crud/get.js
+++ AppBackend/controller/dataset/crud/get.js
@@ -1,46 +1,36 @@
 const csv = require('csv-parser');
+const neatCsv = require('neat-csv');
 const fs = require('fs');
 const path = require('path');
 
 const directoryPath = path.join(__dirname, '../../../files/dataset/weather/');
 
-const api = (req, res) => {
+const api = async (req, res) => {
     let dataset_data = [];
 
     fs.readdir(directoryPath, (err, files) =>{
         //handling error
         if (err) {
             return console.log('Unable to scan directory: ' + err);
-        } 
+        }
         //listing all files using forEach
         files.forEach(function (file) {
-            let datasetSize = 0;
+            var datasetSize
             let filePath = directoryPath + file
             var filename = path.basename(filePath, '.csv')
 
-            fs.createReadStream(filePath)
-            .pipe(csv())
-            .on('data', (row) => {
-                datasetSize += 1;
-            })
-            .on('end', () => {
-                console.log('data size: ', datasetSize);
-                const datasetInfo = {
-                    filename,
-                    size: datasetSize
+            const test = fs.readFile(filePath, async (err, data) => {
+                if(err) {
+                    console.log(err)
+                    return
                 }
-    
-                dataset_data.push(datasetInfo)
-                res.json(dataset_data)
-            });
-
-            
-
-            // var data = JSON.parse(fs.readFileSync(filePath));
-            // model_data.push(data)
-            // console.log(data)
+                const neatData = await neatCsv(data)
+                return datasetSize = neatData.length
+            })
+            console.log(test)
 
         });
+        res.json('get')
         
     });
     
